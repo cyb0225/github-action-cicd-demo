62f2f708ed12754cfb78de97b5878d81735289e6
cyb <2103561941@qq.com>
2023-02-18 10:31:14 +0800

    check reviewdog

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	arr := []int{1, 2, 3, 4, 5}
+
+	arr := []int{1, 2, 3, 4, 5, 6}
 	for _, v := range arr {
 		go func() {
 			// There is a problem with closed packages here
