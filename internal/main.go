e7a512f71b13650f2c966a10131f04bfe5204eff
cyb <2103561941@qq.com>
2023-02-19 11:11:43 +0800

    check

---
+++ internal/main.go
@@ -0,0 +1,17 @@
+/**
+@author: yeebing
+@date: 2023/2/19
+**/
+
+package main
+
+// unused var
+var A = 10
+
+func main() {
+	FuncA()
+}
+
+func FuncA() error {
+	return nil
+}
--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-
 	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	var wg sync.WaitGroup
+	wg.Add(len(arr))
+
 	for i, v := range arr {
 		go func() {
+			defer wg.Done()
 			// There is a problem with closed packages here
 			fmt.Println(i, v)
 		}()
 	}
 
+	wg.Wait()
 }
